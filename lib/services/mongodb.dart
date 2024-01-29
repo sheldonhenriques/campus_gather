fa061116961f9884b25d4dee0b5bcc23b5f96283
Sheldon Henriques <32040506+sheldonhenriques@users.noreply.github.com>
2024-01-29 14:49:13 -0800

    [CAMPUS-5] rearrange the files

---
+++ lib/services/mongodb.dart
@@ -0,0 +1,17 @@
+import 'package:campus_gather/services/event.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class MongoDatabase {
+  static connect() async {
+    String mongoURL = dotenv.get('MONGO_URL');
+    var db = await Db.create(mongoURL);
+    await db.open();
+
+    final eventRepository = EventRepository(db);
+    final events = Events(eventRepository);
+    final allEvents = await events.getEvents();
+
+    await db.close();
+  }
+}
